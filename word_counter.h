194b75a98f84ae7318faef940844b78593f8df6d
Stanislava Gera <stasya.gera@yandex.ru>
2017-06-14 23:16:34 +0300

    bug fixed

--- word_counter.h
+++ word_counter.h
@@ -2,9 +2,10 @@
 #define ASM_COURSE_WORD_COUNTER_H
 
 #include <emmintrin.h>
+#include <chrono>
 
 const char ws = ' ';
-bool ws_seq = true;
+bool ws_seq = false;
 
 int naive_count(const char * text, size_t size) {
     int result = 0;
@@ -19,11 +20,15 @@ int naive_count(const char * text, size_t size) {
     return result;
 }
 
+int naive_count(std::string input) {
+    return naive_count(input.c_str(), input.size());
+}
+
 int count(std::string input) {
     const char *text = input.c_str();
     size_t size = input.size();
 
-    int result = 0;
+    int result = text[0] == ws ? 0 : 1;
     size_t offset = (size_t)text % 16;
     if (offset != 0) {
         offset = 16 - offset;
@@ -46,18 +51,17 @@ int count(std::string input) {
                 "palignr    $1, %1, %2\n"
                 "pandn      %1, %2\n"
                 "pmovmskb   %2, %4\n"
+
         : "=r" (text), "=x" (curr), "=x" (next), "=x" (spaces), "=r" (a)
         : "0" (text), "1" (curr), "2" (next), "3" (spaces), "4" (a)
         : "memory", "cc"
         );
 
         result += __builtin_popcount(a);
+        ws_seq = (text[-1] == ws);
     }
 
-    ws_seq = false;
-    result += naive_count(text, size);
-
-    return result;
+    return result + naive_count(text, size);
 }
 
 #endif //ASM_COURSE_WORD_COUNTER_H
