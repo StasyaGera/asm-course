baf806ef682dccdc4e51bb81637c10d822bf784e
Stanislava Gera <stasya.gera@yandex.ru>
2017-07-01 14:17:55 +0300

    fixed alignments

--- my_memcpy.h
+++ my_memcpy.h
@@ -10,7 +10,7 @@ void naive_memcpy(void* dst, void const* src, size_t size) {
 }
 
 void my_memcpy(void* dst, void const* src, size_t size) {
-    size_t offset = (size_t)src % 16;
+    size_t offset = (size_t)dst % 16;
     if (offset != 0) {
         offset = 16 - offset;
         naive_memcpy(dst, src, offset);
@@ -24,13 +24,14 @@ void my_memcpy(void* dst, void const* src, size_t size) {
     if (size >= 16) {
         __asm__ volatile(
         "cpy:\n"
-                "movdqa     (%1), %3\n"
+                "movdqu     (%1), %3\n"
                 "movntdq    %3, (%0)\n"
                 "add        $16, %0\n"
                 "add        $16, %1\n"
                 "sub        $16, %2\n"
                 "cmp        $16, %2\n"
                 "jnl        cpy\n"
+                "sfence\n"
         : "=r" (dst_cp), "=r" (src_cp), "=r" (size), "=x" (tmp)
         : "0" (dst_cp), "1" (src_cp), "2" (size)
         : "memory", "cc"
